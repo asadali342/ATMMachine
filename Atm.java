019b418fda45bd40cd8a6de4ad363d8e98878ddc
asadali342 <49551836+asadali342@users.noreply.github.com>
2019-04-17 11:02:35 +0500

    Some Changing
    Some Changing for to make Sequence in code..

--- Atm.java
+++ Atm.java
@@ -3,24 +3,28 @@ class Atm{
 	public static void main(String ar[]){
 		Scanner input=new Scanner(System.in);
 		int amount;
+		int fifhun;
 		int hun;
 		int fifty;
 		int twenty;
-		int fifhun;
 		int ten;
 
 		System.out.println("Enter an amount");
 		amount=input.nextInt();
-		ten=amount/10;
+		fifhun=amount/500;
 		hun=amount/100;
 		fifty=amount/50;
 		twenty=amount/20;
-		fifhun=amount/500;
-		System.out.println("Total amount of ten \t"+ten);
-		System.out.println("Total amount of twenty\t"+twenty);
+		ten=amount/10;
+		System.out.println("Total amount of fifhun\t"+fifhun);
 		System.out.println("Total amount of hun\t"+hun);
 		System.out.println("Total amount of fifty\t"+fifty);
-		System.out.println("Total amount of fifhun\t"+fifhun);
+		System.out.println("Total amount of twenty\t"+twenty);
+		System.out.println("Total amount of ten \t"+ten);
+		
+		
+		
+		
 
 
 	}
